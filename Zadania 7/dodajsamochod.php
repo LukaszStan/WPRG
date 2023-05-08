7c46691ee921e9b13b2ca86ce156543780f7f3c1
LukaszStan <s27131@pjwstk.edu.pl>
2023-05-08 20:43:32 +0100

    Zadania 7

---
+++ Zadania 7/dodajsamochod.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dodaj samochód</title>
+</head>
+<body>
+<a href="wszystkiesamochody.php">Powrót</a>
+<h2>Dodaj samochód</h2>
+<form method="post">
+    Marka: <input type="text" name="marka"><br><br>
+    Model: <input type="text" name="model"><br><br>
+    Cena:  <input type="number" name="cena" min="1"><br><br>
+    Rok: <input type="number" name="rok" min="1886"><br><br>
+    Opis: <input type="text" name="opis"><br><br>
+    <input type="submit" value="Dodaj">
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $marka = $_POST['marka'];
+        $model = $_POST['model'];
+        $cena = $_POST['cena'];
+        $rok = $_POST['rok'];
+        $opis = $_POST['opis'];
+
+        $conn = mysqli_connect("localhost","root","","mojabaza");
+        $zap1 = "INSERT INTO samochody(marka,model,cena,rok,opis) VALUES ('${marka}','${model}','${cena}','${rok}','${opis}')";
+        if(!$wyn = mysqli_query($conn,$zap1)){
+            echo 'Błąd, auto nie zostało dodane';
+        }
+        else{
+            echo 'Auto zostało dodane do bazy';
+        }
+        mysqli_close($conn);
+    }
+    ?>
+</form>
+</body>
+</html>
---
+++ Zadania 7/wszystkiesamochody.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wszystkie samochody</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td><a href="index.php">Strona główna</a> </td>
+        <td><a href="wszystkiesamochody.php">Wszystkie samochody</a> </td>
+        <td><a href="dodajsamochod.php">Dodaj samochód</a> </td>
+    </tr>
+</table> <br>
+
+<table>
+    <tr>
+        <td>id</td>
+        <td>marka</td>
+        <td>model</td>
+        <td>cena</td>
+    </tr>
+    <?php
+    $conn = mysqli_connect("localhost","root","","mojabaza");
+    $zap1 = "SELECT * FROM samochody ORDER BY rok";
+    $wyn = mysqli_query($conn,$zap1);
+
+    while($row = mysqli_fetch_array($wyn)){
+        echo
+        "
+        <tr>
+        <td><a href='szczegoly.php?id=${row['id']}'>${row['id']}</a></td>
+        <td>${row['marka']}</td>
+        <td>${row['model']}</td>
+        <td>${row['cena']}</td>
+        </tr>
+        ";
+    }
+    mysqli_close($conn);
+    ?>
+</table>
+</body>
+</html>
